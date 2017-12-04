07bb8f61faf6be2f28f6272a6ab2fcef265f97ac
Naoki Kanazawa <kanazawa@naoki912.net>
2017-12-04 22:58:03 +0900

    [Update] GiftCode Resource

--- src/resource/gift_code.py
+++ src/resource/gift_code.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from flask import make_response
 from flask_restful import Resource
 from flask_restful import Api
 from pony.orm import db_session
+from pony.orm import select
+from pony.orm.serialization import to_json
 
 from model import GiftCode
 
@@ -12,16 +15,19 @@ api = Api(app)
 
 class GiftCodeResource(Resource):
     @db_session
-    def get(self, id: int):
-        a = GiftCode[id]
+    def get(self, id: int=None):
         if id is None:
-            return "hello"
+            return make_response(
+                to_json(select(i for i in GiftCode).order_by(GiftCode.id)),
+                200
+            )
         else:
-            return a
+            gift_code = GiftCode[id]
+            return make_response(to_json(gift_code), 200)
 
     @db_session
     def post(self):
-        gift_code = GiftCode()
+        GiftCode()
 
 
 api.add_resource(
