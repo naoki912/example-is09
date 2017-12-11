af250726fb6c46577049507d3d09089b11002d05
Naoki Kanazawa <kanazawa@naoki912.net>
2017-12-12 07:41:14 +0900

    全てのmodelをFlask-Adminに追加

--- src/app.py
+++ src/app.py
@@ -9,21 +9,29 @@ from resource.product import product_bp
 from config import Config
 from model import db
 from model.gift_code import GiftCode
+from model.product import Product
+from model.settlement import Settlement
+from model.transition import Transition
 from model_schema import ma
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-admin = Admin(app, name='carrier-design', template_mode='bootstrap3')
-admin.add_view(ModelView(GiftCode, db.session))
-
 db.init_app(app)
-
 with app.app_context():
     db.create_all()
 
 ma.init_app(app)
+
+admin = Admin(template_mode='bootstrap3')
+admin.add_views(
+    ModelView(GiftCode, db.session, name="gift_code", endpoint="gift_code_admin"),
+    ModelView(Product, db.session, name="product", endpoint="product_admin"),
+    ModelView(Settlement, db.session, name="settlement", endpoint="settlement_admin"),
+    ModelView(Transition, db.session, name="transition", endpoint="transition_admin"),
+)
+admin.init_app(app)
 
 app.register_blueprint(gift_code_bp, url_prefix='/api/v1')
 app.register_blueprint(settlement_bp, url_prefix='/api/v1')
