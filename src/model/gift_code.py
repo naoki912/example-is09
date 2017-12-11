aa90c53c2303863099b5e9e9e4feeb1c237bd2d5
Naoki Kanazawa <kanazawa@naoki912.net>
2017-12-11 21:24:50 +0900

    model/gift_codeのコメント書き換えた

--- src/model/gift_code.py
+++ src/model/gift_code.py
@@ -5,10 +5,9 @@ from model.mixin import TimestampMixin
 class GiftCode(db.Model, TimestampMixin):
     __tablename__ = 'gift_code'
     __table_args__ = {'sqlite_autoincrement': True}
-    # primary_keyは自動的に`autoincrement=True`が設定されるはずなんだけどダメっぽい
-    # https://stackoverflow.com/questions/20848300/
-    # https://stackoverflow.com/questions/34581905/
     # ToDo: sqliteとMySQLの両方に対応させる
+    # sqliteだとBIGINTでautoincrementが使用できないので対応する
+    # https://stackoverflow.com/questions/18835740/does-bigint-auto-increment-work-for-sqlalchemy-with-sqlite
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     code = db.Column(db.String(16), unique=True, nullable=False)
     balance = db.Column(db.Integer, nullable=False)
